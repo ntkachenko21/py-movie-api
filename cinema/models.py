a65a1f6cc54bcc9041ebae085f54963bcbbf2976
Nikita Tkachenko <7heswatgg@gmail.com>
2025-04-28 20:00:21 +0200

    add the missing files

---
+++ cinema/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    duration = models.IntegerField()
+
+    class Meta:
+        verbose_name_plural = "movies"
+
+
+    def __str__(self):
+        return f"Title: {self.title}. ({self.duration} min)"
